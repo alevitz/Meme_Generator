d73294f54b649be9ac93ffca8eca040ac9b4bd75
alevitz <50308013+alevitz@users.noreply.github.com>
2019-12-05 16:25:54 -0800

    Fixed Removal Button Option

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const addItemTextBottom = document.querySelector('input#addItemTextBottom');
 const addItemTextTop = document.querySelector('input#addItemTextTop');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const imgDiv = document.querySelector('#imageDiv');
 
 addItemButton.addEventListener('click', () => {
     let parentDiv = document.getElementById('imageDiv');
     let div = document.createElement('div');
     let divTopText = document.createElement('div');
     let divBottomText = document.createElement('div');
+    let divRemoveButton = document.createElement('button');
     let img = document.createElement('img');
     img.src = addItemUrl.value;
     parentDiv.appendChild(div);
     div.appendChild(img);
     div.appendChild(divTopText);
     div.appendChild(divBottomText);
+    div.appendChild(divRemoveButton);
     div.className = 'container';
     divTopText.className = 'topText'
     divBottomText.className = 'bottomText'
+    divRemoveButton.textContent = "Remove Meme"
+    divRemoveButton.className = 'removeButton'
     divTopText.textContent = addItemTextTop.value;
     divBottomText.textContent = addItemTextBottom.value;
     addItemUrl.value = '';
@@ -25,9 +30,11 @@ addItemButton.addEventListener('click', () => {
     addItemTextTop.value = '';
 });
 
-
-removeItemButton.addEventListener('click', () => {
-    let parentDiv = document.getElementById('imageDiv');
-    let div = document.querySelector('.container:last-child');
-    parentDiv.removeChild(div);
+imgDiv.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+        let div = event.target.parentNode;
+        let body = div.parentNode;
+        body.removeChild(div);
+    }
 });
+
